bbb60666952cd2aca9c9c4c66620c3a091dc293e
Ron Rider <rideron89@gmail.com>
2018-01-22 18:10:26 -0500

    adds /load route

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,6 @@ use App\Router;
 
 Router::get('/', 'HomeController@index');
 Router::get('/test', 'HomeController@test');
+Router::get('/load', 'HomeController@load');
 
 Router::post('/', 'HomeController@test');
