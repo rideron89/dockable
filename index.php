facd49ff720872575dd2eefd76ab0e69d2ef22eb
Ron Rider <rideron89@gmail.com>
2018-01-22 18:09:57 -0500

    add formatted comment to /index.php

--- index.php
+++ index.php
@@ -4,5 +4,10 @@ require __DIR__ . '/vendor/autoload.php';
 
 require __DIR__ . '/app/Application.php';
 
+/*------------------------------------------------------------*
+ |
+ |    Load the application and start it up!
+ |
+ *------------------------------------------------------------*/
 $app = new App\Application();
 $app->start();
