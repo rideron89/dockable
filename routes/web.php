d1c1c726ce9065bb3aaaa9ffb4bb7fa455a7a7b0
Ron Rider <rideron89@gmail.com>
2018-01-24 17:50:39 -0500

    prettify routes file

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,28 @@
 <?php
 
-use App\Router;
+use App\Routing\Router;
 
+/*------------------------------------------------------------*
+ |
+ |    Front-end end-points
+ |
+ *------------------------------------------------------------*/
 Router::get('/', 'HomeController@index');
-Router::get('/sources', 'HomeController@sources');
 
-Router::generateRoutes('/api/sources', 'SourcesController', 'source_id');
+/*------------------------------------------------------------*
+ |
+ |    Sources API end-points
+ |
+ *------------------------------------------------------------*/
+Router::get(   '/api/sources',             'SourcesController@index' )->middleware('BasicAuth');
+Router::get(   '/api/sources/{source_id}', 'SourcesController@read'  )->middleware('BasicAuth');
+Router::post(  '/api/sources',             'SourcesController@create')->middleware('BasicAuth');
+Router::put(   '/api/sources/{source_id}', 'SourcesController@update')->middleware('BasicAuth');
+Router::delete('/api/sources/{source_id}', 'SourcesController@delete')->middleware('BasicAuth');
+
+/*------------------------------------------------------------*
+ |
+ |    Auth API end-points
+ |
+ *------------------------------------------------------------*/
+Router::get('/api/auth', 'AuthController@index')->middleware(['BasicAuth']);
