b856d1fd2af0073afe132e12b955459c45379cab
Ron Rider <rideron89@gmail.com>
2018-01-25 11:52:05 -0500

    rename Result error to err

--- app/Databases/Result.php
+++ app/Databases/Result.php
@@ -9,7 +9,7 @@ class Result
     *
     * @var string
     */
-    public $error;
+    public $err;
 
     /**
     * Holds all the results from the database operation.
@@ -18,18 +18,10 @@ class Result
     */
     public $documents;
 
-    /**
-    * The number of results returned by the database operation.
-    *
-    * @var int
-    */
-    public $count;
-
     public function __construct($results, $error = false)
     {
         $this->documents = $results;
-        $this->count = count($results);
 
-        $this->error = $error;
+        $this->err = $error;
     }
 }
