ab994f15a0ccfa79f3560ad4e65acc78e9d4876c
Ron Rider <rideron89@gmail.com>
2018-02-16 15:30:20 -0500

    remove auth cookie if its been unauthorized

--- app/Controllers/TokenController.php
+++ app/Controllers/TokenController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Databases\MongoClient;
+use App\Services\CookieManagerService;
 use MongoDB\BSON\ObjectId;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,7 +12,15 @@ class TokenController
 {
     public function delete(Request $request)
     {
+        $auth = CookieManagerService::get('auth');
+        $auth = base64_decode(trim($auth));
+        $auth = json_decode($auth, true);
+
         $token_id = $request->params['token_id'];
+
+        if ($auth['id'] === $token_id) {
+            CookieManagerService::remove('auth');
+        }
 
         if (!$token_id) {
             return new Response('Bad Request', Response::HTTP_BAD_REQUEST);
